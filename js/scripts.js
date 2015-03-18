50e6238321c3023d89ad2c54bc0a1598b0d2d645
Sean Holstrom <basementbebop@gmail.com>
2015-03-17 18:24:05 -0700

    Add function to collect json data

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,17 @@
 $(document).ready(function() {
-  
+  var apiKey = '70433fafb74c3e90db90f265a6c0dced',
+      baseUrl = 'https://api.forecast.io/forecast/',
+      pdxLat = 45.5200,
+      pdxLong = -122.6819;
+
+  function getWeather() {
+    var callUrl = baseUrl + apiKey + "/" + pdxLat + "," + pdxLong + "?callback=?";
+    $.getJSON(callUrl, function(d) {
+      var testTemp = d.hourly.data[0].temperature;
+      console.log();
+    });
+  }
+
+  getWeather();
+
 }); // end ready
